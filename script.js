4e2cdd59921760dd290ad2c7c7ec44240c702e74
Practice-Here <140873521+Practice-Here@users.noreply.github.com>
2026-07-01 22:40:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
 
     var typed = new Typed(".typing-2", {
     strings: ["QA Engineer!", "Manual Tester!", "Automation Tester!"],
-    typeSpeed: 250,
+    typeSpeed: 200,
     backSpeed: 200,
     loop: true
     });
