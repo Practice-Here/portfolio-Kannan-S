530a2a193b1386650cede407899226745c056c63
Practice-Here <140873521+Practice-Here@users.noreply.github.com>
2026-07-21 13:07:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ $(document).ready(function () {
 
     // Typing text animation script (merged for .typing & .typing-2)
     var typed = new Typed(".typing", {
-    strings: ["QA Engineer!", "Manual Tester!", "Automation Tester!"],
+    strings: ["Software Tester!", "Manual Tester!", "Automation Tester!"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
     });
 
     var typed = new Typed(".typing-2", {
-    strings: ["QA Engineer!", "Manual Tester!", "Automation Tester!"],
+    strings: ["Software Tester!", "Manual Tester!", "Automation Tester!"],
     typeSpeed: 200,
     backSpeed: 200,
     loop: true
